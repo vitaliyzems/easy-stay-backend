37bcf3d615966fffd9e6ca4a853052e95057ecde
Vitaliy <vitalyazems@gmail.com>
2025-06-27 12:01:52 +0500

    fixed token error

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes');
 const cors = require('cors');
-
-require('dotenv').config();
 
 const app = express();
 
