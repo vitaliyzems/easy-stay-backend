c6c246a3367e705ef4695bc82492f1796e8bd951
Vitaliy <vitalyazems@gmail.com>
2025-06-28 11:52:09 +0500

    changed cookie options during auth

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,12 @@ router.post('/register', async (req, res) => {
     );
 
     res
-      .cookie('token', token, { httpOnly: true })
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'None',
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      })
       .send({ error: null, user: mapUser(user) });
   } catch (error) {
     res.send({ error: error.message || 'Unknown error' });
@@ -25,7 +30,12 @@ router.post('/login', async (req, res) => {
     const { user, token } = await login(req.body.email, req.body.password);
 
     res
-      .cookie('token', token, { httpOnly: true })
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'None',
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      })
       .send({ error: null, user: mapUser(user) });
   } catch (error) {
     res.send({ error: error.message || 'Unknown error' });
@@ -33,7 +43,14 @@ router.post('/login', async (req, res) => {
 });
 
 router.post('/logout', (req, res) => {
-  res.cookie('token', '', { httpOnly: true }).send({});
+  res
+    .cookie('token', '', {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'None',
+      maxAge: 0,
+    })
+    .send({});
 });
 
 module.exports = router;
